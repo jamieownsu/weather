48efb20f186813f76eeaa1f9243e41e3332f457a
Jamie <jamie.a.otoole@gmail.com>
2025-08-30 11:46:35 -0400

    -plural string

--- app/src/main/java/com/chalupin/weather/presentation/home/components/LastUpdatedText.kt
+++ app/src/main/java/com/chalupin/weather/presentation/home/components/LastUpdatedText.kt
@@ -1,16 +1,16 @@
 package com.chalupin.weather.presentation.home.components
 
-import android.text.format.DateUtils
-import androidx.compose.animation.Crossfade
+import android.util.Log
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import com.chalupin.weather.R
 import kotlinx.coroutines.delay
@@ -18,24 +18,31 @@ import kotlinx.coroutines.delay
 @Composable
 fun LastUpdatedText() {
     val startTimeMillis = remember { System.currentTimeMillis() }
-    var elapsedTimeString by remember {
-        mutableStateOf(
-            DateUtils.getRelativeTimeSpanString(startTimeMillis).toString()
-        )
-    }
+    var elapsedMinutes by remember { mutableIntStateOf(0) }
 
     LaunchedEffect(key1 = true) {
         while (true) {
-            elapsedTimeString = DateUtils.getRelativeTimeSpanString(startTimeMillis).toString()
+            val now = System.currentTimeMillis()
+            val diff = now - startTimeMillis
+            val seconds = diff / 1000
+            val minutes = seconds / 60
+            elapsedMinutes = minutes.toInt()
+            Log.d("elapsed", elapsedMinutes.toString())
             delay(1000L)
         }
     }
 
-    Crossfade(targetState = elapsedTimeString, label = "elapsed time crossfade") { text ->
-        Text(
-            stringResource(id = R.string.last_updated, text),
-            style = MaterialTheme.typography.labelSmall,
-            color = Color.Gray,
-        )
-    }
+    Text(
+        if (elapsedMinutes != 0) {
+            pluralStringResource(
+                id = R.plurals.last_updated,
+                elapsedMinutes,
+                elapsedMinutes
+            )
+        } else {
+            stringResource(R.string.updated_just_now)
+        },
+        style = MaterialTheme.typography.labelSmall,
+        color = Color.Gray,
+    )
 }
